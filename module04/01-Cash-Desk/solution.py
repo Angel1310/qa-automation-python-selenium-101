f255a586287fdf0a0ab376290e0479c9b1cf8271
Angel Milanov <angel.milanov1310@gmail.com>
2019-06-27 21:35:19 +0300

    part of module04

--- module04/01-Cash-Desk/solution.py
+++ module04/01-Cash-Desk/solution.py
@@ -1,4 +1,60 @@
-import os
 
-class Bill
-    amount = 0
+class Bill:
+    def __init__(self, amount):
+        if not isinstance(amount, int):
+            raise TypeError("Amount is not a number.")
+        if amount <= 0:
+            raise ValueError("Amount is smaller than zero.")
+        self.amount = amount
+
+    def __str__(self):
+        return "A {}$ bill".format(self.amount)
+
+    def __repr__(self):
+        return "A {}$ bill".format(self.__class__)
+
+    def __int__(self):
+        return self.amount
+
+    def __eq__(self, other):
+        return self.amount == other.amount
+
+    def __hash__(self):
+        return self.amount
+
+
+class BatchBill:
+    def __init__(self, bills):
+        self.bills = bills
+        self.index = len(bills)
+
+    def __len__(self):
+        return len(self.bills)
+
+    def __getitem__(self, item):
+        return self.bills[item]
+
+    def total(self):
+        total = 0
+        for bill in self.bills:
+            total += int(bill)
+
+        return total
+
+class CashDesk:
+
+	def __init__(self):
+		self.bills = []
+
+	def take_money(self, bills):
+		try:
+			for bill in bills:
+				self.bills.append(bill)
+		except:
+			self.bills.append(bills)
+
+	def total(self):
+		result = 0
+		for bill in self.bills:
+			result += int(bill)
+		return result
